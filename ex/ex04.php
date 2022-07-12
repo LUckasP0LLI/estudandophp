210a9cd48305c3773dbdab050b2ae8f85d4535c9
LUckasP0LLI <39037021+LUckasP0LLI@users.noreply.github.com>
2022-07-12 10:43:14 -0300

    Aprendendo uso de laços de repetiçaõ

---
+++ ex/ex04.php
@@ -0,0 +1,25 @@
+<?php
+/*
+$counter = 0; 
+while($counter<=10){ //repete ate a condição. 
+     echo $counter;
+     $counter++;
+}
+*/
+/*
+$counter = 200;
+do{ //executa somente uma vez.
+     echo "$counter<br>"; 
+     $counter++;
+}while($counter<=100);
+*/
+/*
+for($counter=0; $counter<=10; $counter++){//sabe o tempo da interação
+     echo "$counter<br>";
+}
+*/
+for($i=10; $i<=100; $i+=10){
+     echo "$i<br>";
+}
+
+?>
